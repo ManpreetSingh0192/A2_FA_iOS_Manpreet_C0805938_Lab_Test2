22c19385380f54c99a3f9b21f3ab69eabe5a6494
ManpreetSingh0192 <sandhumanpreet634@gmail.com>
2021-05-25 09:53:48 -0700

    Initial Commit

---
+++ A2_FA_iOS_Manpreet_C0805938/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_iOS_Manpreet_C0805938
+//
+//  Created by Manpreet Singh on 2021-05-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
